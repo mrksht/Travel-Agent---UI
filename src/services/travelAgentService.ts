817de75c574beb619aa50e63da3a1e585ba8f741
Rakshit <rakshit@Rakshits-MacBook-Pro.local>
2025-05-27 20:49:54 +0530

    .env added

--- src/services/travelAgentService.ts
+++ src/services/travelAgentService.ts
@@ -1,30 +1,33 @@
-const API_URL = "http://localhost:3000/agent/query";
+const API_URL =
+  import.meta.env.VITE_ENV === "dev"
+    ? "http://localhost:3000/agent/query"
+    : "travel-agent-langgraph-backend-production.up.railway.app/agent/query";
 
 interface QueryAgentRequest {
-    message: string;
-    threadId: number;
+  message: string;
+  threadId: number;
 }
 
 export const queryAgent = async ({ message, threadId }: QueryAgentRequest) => {
-    try {
-        const res = await fetch(API_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ message, threadId }),
-        });
+  try {
+    const res = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message, threadId }),
+    });
 
-        if (!res.ok) {
-            const errorText = await res.text();
-            throw new Error(`Server responded with ${res.status}: ${errorText}`);
-        }
-
-        const data = await res.json();
-        return {
-            text: data.response
-        }
-    } catch (err) {
-        console.error("Error:", err);
+    if (!res.ok) {
+      const errorText = await res.text();
+      throw new Error(`Server responded with ${res.status}: ${errorText}`);
     }
-}
+
+    const data = await res.json();
+    return {
+      text: data.response,
+    };
+  } catch (err) {
+    console.error("Error:", err);
+  }
+};
